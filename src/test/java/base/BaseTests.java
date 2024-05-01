e38c9bf669fa13b170c393db5a2e003953c5f42f
Ap Panta <ap.pantazopoulos@yahoo.com>
2024-05-01 20:46:27 +0300

    add method to take a screenshot when a test fails.
    -also create a folder where the screenshots will be stored.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,18 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
 
@@ -23,6 +31,19 @@ public class BaseTests {
     @AfterClass
     public void teardown(){
         //driver.quit();
+    }
+
+    @AfterMethod
+    public void takeScreenshotIfTestFailed(ITestResult result){
+        if (result.getStatus() == ITestResult.FAILURE){
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/shots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
